c6ba281a1e7ea52b416b41b7564a6f3d33857096
Temesgen Sisay <lijtem@gmail.com>
2019-03-26 21:40:57 +0300

    Product Recieve Unit Cost and Unit Price Added

--- REERP.Models/ProductReceiveLineItem.cs
+++ REERP.Models/ProductReceiveLineItem.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
+﻿using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace REERP.Models
 {
@@ -19,6 +14,10 @@ namespace REERP.Models
         [Range(1, 10000000)]
         [DisplayName("Unit Cost")]
         public decimal UnitCost { get; set; }
+
+        //        [Range(1, 10000000)]
+        //        [DisplayName("Unit Price")]
+        //        public decimal UnitPrice { get; set; }
 
         public int ProductReceiveId { get; set; }
         public ProductReceive ProductReceive { get; set; }
